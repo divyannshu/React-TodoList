e86a942f533da19c67362615ba8acaf6f05b6104
divyannshu <divyanshum3232@gmail.com>
2024-10-13 17:21:14 +0530

    Server set up done

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,41 @@
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
+import axios from 'axios';
 import ListCreate from './ListCreate';
 import ListList from './ListList';
 function App(){
     const [tasks,setTasks]=useState([]);
-    const EditTaskbyId=(newTitle,id)=>{
+    const fetchTasks=async ()=>{
+    const response=await axios.get('http://localhost:3001/tasks');
+
+    setTasks(response.data); 
+ };
+ useEffect(()=>{
+    fetchTasks();
+ },[]);  
+    const EditTaskbyId=async(newTitle,id)=>{
+        const response=await axios.put(`http://localhost:3001/tasks/${id}`,{
+        title:newTitle,
+    });
         const updatedTasks=tasks.map((task)=>{
             if(task.id===id)
-                return {title:newTitle};
+                return {...task,...response.data};
             return task;
         });
         setTasks(updatedTasks);
     }
-    const deleteTaskbyId=(id)=>{
+    const deleteTaskbyId=async(id)=>{
+        await axios.delete(`http://localhost:3001/tasks/${id}`);
         const updatedTasks=tasks.filter((task)=>{
             return task.id!=id;
         })
         setTasks(updatedTasks)
     }
     const createTask=async(title)=>{
+        const response=await axios.post('http://localhost:3001/tasks',{
+        title:title
+    });
         const updatedTasks=[
-            ...tasks,{title:title,id:Math.round(Math.random()*9999)}
+            ...tasks,response.data
         ];
         setTasks(updatedTasks);
     }
--- src/components/ListEdit.js
+++ src/components/ListEdit.js
@@ -13,7 +13,7 @@ function ListEdit({task,onSubmit}){
              <form class="max-w-sm mx-auto" onSubmit={handleSubmit}>
     <div class="mb-5">
     <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter tasks:</label>
-    <input  onChange={handleChange} type="text" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Add tasks" required />
+    <input value={title} onChange={handleChange} type="text" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Add tasks" required />
     </div>
     <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create</button>
     </form>
